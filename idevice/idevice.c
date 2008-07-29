aca03e613902cc36ed990225461c036c791698a9
planetbeing <planetbeing@andLinux.(none)>
2008-07-28 20:32:26 -0400

    Added DFU game

--- idevice/idevice.c
+++ idevice/idevice.c
@@ -20,6 +20,31 @@ static char tmpRestorePath[MAX_PATH];
 
 static OutputState* data = NULL;
 
+static volatile int Stage;
+
+typedef enum {
+	Disconnected,
+	DFUConnected,
+	NormalConnected,
+	RecoveryConnected
+} ConnectStatus;
+
+static volatile ConnectStatus Status;
+
+
+void cleanup_and_exit() {
+	unlink(tmpXALLPath);
+	unlink(tmpWTFPath);
+	unlink(tmpIBSSPath);
+	unlink(tmpRestorePath);
+	rmdir(tmpDFUPath);
+	rmdir(tmpFirmwarePath);
+	rmdir(tmpFilePath);
+	releaseOutput(&data);
+
+	exit(0);
+}
+
 void dfu_progress_callback()
 {
 	XLOG(3, "DFU progress callback...");
@@ -34,6 +59,12 @@ void DoDFU(am_recovery_device *rdev, const char* restoreBundle) {
 	int ret = AMRestorePerformDFURestore( rdev, opts,
 									  (void*)dfu_progress_callback, NULL );
 
+	if(ret != 0) {
+		fprintf(stdout, "I'm sorry, but something seems to have gone wrong during the download. Please try connecting your iPhone/iPod to another USB port (NOT through a hub) and trying again.\n");
+		fflush(stdout);
+		cleanup_and_exit();
+	}
+
 	XLOG(3, "AMRestorePerformDFURestore: %d", ret);
 
 	CFRelease(value);
@@ -47,15 +78,31 @@ void notification(struct am_device_notification_callback_info *info)
 	
 	if(msg == ADNCI_MSG_CONNECTED)
 	{
+		Status = NormalConnected;
 		XLOG(3, "device connected in normal mode");
+
+		if(Stage == 2) {
+			fprintf(stdout, "Sorry, but you did not follow the instructions correctly. Please try again.\n");
+			cleanup_and_exit();
+		}
 	} else if ( msg == ADNCI_MSG_DISCONNECTED ) {
+		Status = Disconnected;
 		XLOG(3, "device disconnected in normal mode");
 	}
 }
 
 void recovery_connect_callback(am_recovery_device *rdev)
 {
+	Status = RecoveryConnected;
 	XLOG(3, "device connected in recovery mode");
+
+	if(Stage == 2) {
+		fprintf(stdout, "Sorry, but you did not follow the instructions correctly. Please try again.\n");
+		cleanup_and_exit();
+	} else if(Stage != 3) {
+		return;
+	}
+
 	XLOG(3, "sendCommandToDevice(setenv auto-boot) returned: %d", sendCommandToDevice(rdev, CFSTR("setenv auto-boot true"), 0));
 	XLOG(3, "sendCommandToDevice(saveenv) returned: %d", sendCommandToDevice(rdev, CFSTR("saveenv"), 0));
 	XLOG(3, "sendCommandToDevice(setenv idle-off) returned: %d", sendCommandToDevice(rdev, CFSTR("setenv idle-off false"), 0));
@@ -63,45 +110,50 @@ void recovery_connect_callback(am_recovery_device *rdev)
 	XLOG(3, "sendCommandToDevice(setpicture) returned: %d", sendCommandToDevice(rdev, CFSTR("setpicture 0"), 0));
 	XLOG(3, "sendCommandToDevice(bgcolor) returned: %d", sendCommandToDevice(rdev, CFSTR("bgcolor 0 0 0"), 0));
 
-	unlink(tmpXALLPath);
-	unlink(tmpWTFPath);
-	unlink(tmpIBSSPath);
-	unlink(tmpRestorePath);
-	rmdir(tmpDFUPath);
-	rmdir(tmpFirmwarePath);
-	rmdir(tmpFilePath);
-	releaseOutput(&data);
+	fprintf(stdout, "Please use iTunes to restore your iPhone/iPod with a custom IPSW now. You may now let go of the home button.\n");
+	fflush(stdout);
 
-	exit(0);
+	cleanup_and_exit();
 }
 
 void recovery_disconnect_callback(am_recovery_device *rdev)
 {
+	Status = Disconnected;
 	XLOG(3, "device disconnected in recovery mode");
 }
 
 void dfu_connect_callback(am_recovery_device *rdev)
 {
+	Status = DFUConnected;
 	XLOG(3, "device connected in dfu mode");
+	if(Stage != 3) {
+		Stage = 3;
+		fprintf(stdout, "Congratulations! You have successfully entered DFU mode. Please wait while your iPhone/iPod is being prepared to accept custom IPSWs...\n");
+		fflush(stdout);
+	}
 	DoDFU(rdev, extractedIPSWPath);
 }
 
 void dfu_disconnect_callback(am_recovery_device *rdev)
 {
+	Status = Disconnected;
 	XLOG(3, "device disconnected in dfu mode");
+}
+
+void logCB(const char* Message) {
+	printf("%s\n", Message);
 }
 
 int main(int argc, char* argv[])
 {
 	init_libxpwn();
+	libxpwn_log(logCB);
+	libxpwn_loglevel(0);
 
 	printf("---------------------------PLEASE READ THIS---------------------------\n");
 	printf("Please make certain that all iTunes related processes are not running\n");
-	printf("at this time (use Task Manager, etc. to end them). Your iPhone/iPod\n");
-	printf("must be placed into DFU mode AFTER iTunes had been turned off. This\n");
-	printf(" will allow me to talk to it without iTunes getting in beforehand.\n");
-	printf("USB Product ID of iPhone ought to be 0x1222\n");
-	printf("---------------------------PLEASE READ THIS---------------------------\n");
+	printf("at this time (use Task Manager, etc. to end them).\n");
+	printf("---------------------------PLEASE READ THIS---------------------------\n\n\n");
 
 	if(argc < 4) {
 		printf("usage: %s <custom.ipsw> <n82ap|m68ap|n45ap> <image-to-display.img3>\n", argv[0]);
@@ -109,6 +161,8 @@ int main(int argc, char* argv[])
 		return 0;
 	}
 
+	Stage = 0;
+	Status = Disconnected;
 
 	char ibssName[100];
 	char wtfName[100];
@@ -206,9 +260,13 @@ int main(int argc, char* argv[])
 	extractedIPSWPath = tmpFilePath;
 	bootImagePath = argv[3];
 	
+
+	fprintf(stdout, "\nGetting iPhone/iPod status...\n");
+	fflush(stdout);
+
 	if(LoadWindowsDLL() < 0) {
 		printf("Failed to load iTunes Mobile Device driver!\n");
-		return 1;
+		cleanup_and_exit();
 	}
 	
 	mach_error_t ret;
@@ -217,7 +275,7 @@ int main(int argc, char* argv[])
 	ret = AMDeviceNotificationSubscribe(notification, 0, 0, 0, &notif);
 	if(ret < 0) {
 		printf("Failed to subscribe for device notifications!\n");
-		return 1;
+		cleanup_and_exit();
 	}
 	
 	ret = AMRestoreRegisterForDeviceNotifications(
@@ -230,9 +288,124 @@ int main(int argc, char* argv[])
 						
 	if(ret < 0) {
 		printf("Failed to subscribe for device restore notifications!\n");
-		return 1;
+		cleanup_and_exit();
 	}
-	
+
+
+	sleep(2);
+
+	char responseBuffer[10];
+	int countdown;
+
+	if(Status == Disconnected) {
+connectDevice:
+		fprintf(stdout, "Is your iPhone/iPod connected to your computer via USB?\n");
+		fprintf(stdout, "Please answer (y/n): ");
+		fflush(stdout);
+		fgets(responseBuffer, 10, stdin);
+		if(responseBuffer[0] == 'y' || responseBuffer[0] == 'Y') {
+			goto beginDFU;
+		} else if(responseBuffer[0] == 'n' || responseBuffer[0] == 'N') {
+			fprintf(stdout, "Please connect your iPhone/iPod to your computer\n");
+			fprintf(stdout, "Press enter when you have connected your iPhone/iPod... ");
+			fflush(stdout);
+			fgets(responseBuffer, 10, stdin);
+			sleep(2);
+			if(Status != Disconnected) {
+				goto turnOffDevice;
+			} else {
+isPoweringOn:
+				fprintf(stdout, "Is your iPhone currently powering on?\n");
+				fprintf(stdout, "Please answer (y/n): ");
+				fflush(stdout);
+				fgets(responseBuffer, 10, stdin);
+				if(responseBuffer[0] == 'y' || responseBuffer[0] == 'Y') {
+					fprintf(stdout, "Waiting for iPhone/iPod to power on...\n");
+					fflush(stdout);
+					while(Status == Disconnected) {
+						sleep(1);
+					}
+					goto turnOffDevice;
+				} else if(responseBuffer[0] == 'n' || responseBuffer[0] == 'N') {
+					goto beginDFU;
+				} else {
+					goto isPoweringOn;
+				}
+			}
+		} else {
+			goto connectDevice;
+		}
+	} else {
+turnOffDevice:
+		fprintf(stdout, "Please turn off your iPhone/iPod without disconnecting the cable connecting it to the computer\n");
+		fprintf(stdout, "Press enter when you have turned off your iPhone/iPod... ");
+		fflush(stdout);
+		fgets(responseBuffer, 10, stdin);
+
+		fprintf(stdout, "Waiting for iPhone/iPod to power off...\n");
+		fflush(stdout);
+		while(Status != Disconnected) {
+			sleep(1);
+		}
+	}
+
+beginDFU:
+	fprintf(stdout, "Timing is crucial for the following tasks. I will ask you to do the following:\n");
+	fprintf(stdout, "\t1. Press and hold down the power button for five seconds\n");
+	fprintf(stdout, "\t2. Without letting go of the power button, press and hold down the power AND home buttons for ten seconds\n");
+	fprintf(stdout, "\t3. Without letting go of the home button, release the power button\n");
+	fprintf(stdout, "\t4. Wait 30 seconds while holding down the home button\n");
+	fprintf(stdout, "It is crucial that you follow instructions to the letter. Are you ready to begin?\n");
+	fprintf(stdout, "Please answer (y/n): ");
+	fflush(stdout);
+	fgets(responseBuffer, 10, stdin);
+	if(responseBuffer[0] != 'y' && responseBuffer[0] != 'Y')
+		goto beginDFU;
+
+	Stage = 2;
+
+	for(countdown = 5; countdown > 0; countdown--) {
+		fprintf(stdout, "Beginning process in %d seconds...\n", countdown);
+		fflush(stdout);
+		sleep(1);
+	}
+
+	fprintf(stdout, "\nPress and hold down the POWER button (you should now be just holding the power button)... ");
+	fflush(stdout);
+
+	for(countdown = 5; countdown > 0; countdown--) {
+		fprintf(stdout, "%d... ", countdown);
+		fflush(stdout);
+		sleep(1);
+	}
+
+
+	fprintf(stdout, "\n\nPress and hold down the HOME button, DO NOT LET GO OF THE POWER BUTTON (you should now be just holding both the power and home buttons)... ");
+	fflush(stdout);
+
+	for(countdown = 10; countdown > 0; countdown--) {
+		fprintf(stdout, "%d... ", countdown);
+		fflush(stdout);
+		sleep(1);
+	}
+
+	fprintf(stdout, "\n\nRelease the POWER button, DO NOT LET GO OF THE HOME BUTTON (you should now be just holding the home button)... ");
+	fflush(stdout);
+
+	for(countdown = 30; countdown > 0; countdown--) {
+		if(Status != Disconnected)
+			goto waitForFinish;
+
+		fprintf(stdout, "%d... ", countdown);
+		fflush(stdout);
+		sleep(1);
+	}
+
+	fprintf(stdout, "\n\nEither you did not follow instructions correctly or your USB hardware is malfunctioning. Please use another USB port to connect your iPhone/iPod (NOT through a USB hub) and consider restarting your computer before trying again.\n");
+	fflush(stdout);
+	cleanup_and_exit();
+
+waitForFinish:
 	while(1) {
 		msleep(1);
 	}
